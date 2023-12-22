edf451e4888fb12f64498aca30d52acfcec0db78
Hardikgiri Gosai <hardik.gosai007@gmail.com>
2023-12-22 11:16:38 +0530

    Add project files.

---
+++ Getri_MVC_Consume_API/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Getri_MVC_Consume_API.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Getri_MVC_Consume_API.Controllers
+{
+    public class UserController : Controller
+    {
+        HttpClient _client;
+        IConfiguration configuration;
+
+        public UserController(IConfiguration _configuration)
+        {
+            configuration = _configuration;
+            string apiAddress = configuration["ApiAddress"];
+            Uri baseAddress = new Uri(apiAddress);
+            _client = new HttpClient
+            {
+                BaseAddress = baseAddress
+            };
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<UsersViewModel> users = new List<UsersViewModel>();
+            HttpResponseMessage response = await _client.GetAsync("api/User/GetAllUsers");
+            if (response.IsSuccessStatusCode)
+            {
+                var result = response.Content.ReadAsStringAsync().Result;
+                users = JsonConvert.DeserializeObject<List<UsersViewModel>>(result);
+            }
+            return View(users);
+        }
+    }
+}
---
+++ Getri_MVC_Consume_API/ViewModel/UsersViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Getri_MVC_Consume_API.ViewModel
+{
+    public class UsersViewModel
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Email Address is required")]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name ="Last Name")]
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string ContactNo { get; set; }
+
+        [HiddenInput]
+        public long Id { get; set; }
+
+        [Display(Name = "Created Date")]
+        public DateTime ModifiedDate { get; set; }
+
+        public string IPAddress { get; set; }
+    }
+}
